4aca5dfaccaa39fb6a3ef7bb925e53fe9e137939
Alexander Sae <gerritadmin@localdomain>
2022-06-05 15:50:58 +0300

    Add new tests

---
+++ tst/LeapYearFixtureTests.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+#include <LeapYear.h>
+
+#include <iostream>
+
+class LeapYearFixtureTests : public ::testing::Test {
+protected:
+    LeapYear leapYear;
+};
+
+TEST_F(LeapYearFixtureTests, 1IsOdd_IsNotLeapYear) {
+    ASSERT_FALSE(leapYear.isLeap(1));
+}
+
+TEST_F(LeapYearFixtureTests, 711IsOdd_IsNotLeapYear) {
+    ASSERT_FALSE(leapYear.isLeap(711));
+}
+
+TEST_F(LeapYearFixtureTests, 1989IsOdd_IsNotLeapYear) {
+    ASSERT_FALSE(leapYear.isLeap(1989));
+}
+
+TEST_F(LeapYearFixtureTests, 2013IsOdd_IsNotLeapYear) {
+    ASSERT_FALSE(leapYear.isLeap(2013));
+}
