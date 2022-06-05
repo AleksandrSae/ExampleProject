ede62074b4ac8a31d74ec12a2001a4db85164dd4
Alexander Sae <gerritadmin@localdomain>
2022-06-05 14:15:34 +0300

    Initial commit

---
+++ build/tst/CMakeFiles/ExampleProject_tst.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alexander/temp/test_sample/ExampleProject/tst/Formula_test.cpp" "/home/alexander/temp/test_sample/ExampleProject/build/tst/CMakeFiles/ExampleProject_tst.dir/Formula_test.cpp.o"
+  "/home/alexander/temp/test_sample/ExampleProject/tst/main.cpp" "/home/alexander/temp/test_sample/ExampleProject/build/tst/CMakeFiles/ExampleProject_tst.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../lib/googletest/googletest/include"
+  "../lib/googletest/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/alexander/temp/test_sample/ExampleProject/build/src/CMakeFiles/ExampleProject_lib.dir/DependInfo.cmake"
+  "/home/alexander/temp/test_sample/ExampleProject/build/lib/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
